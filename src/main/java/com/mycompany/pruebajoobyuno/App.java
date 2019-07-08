6a59b942e7e818a7584e6c6bb766db1afe887fd2
carsonlrl <eldiadelpecado@gmail.com>
2019-07-08 17:37:26 -0500

    Update App.java
    se le cambio el mensaje de get de nuevo

--- src/main/java/com/mycompany/pruebajoobyuno/App.java
+++ src/main/java/com/mycompany/pruebajoobyuno/App.java
@@ -8,7 +8,7 @@ import org.jooby.Jooby;
 public class App extends Jooby {
 
   {
-    get("/", () -> "Hola Suemy Solano");
+    get("/", () -> "Hola Suemy Solano ya dile que si :v");
   }
 
   public static void main(final String[] args) {
