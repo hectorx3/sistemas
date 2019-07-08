25923759e0b38169599d206fa524348e73fc9f88
Suemyc <52679437+Suemyc@users.noreply.github.com>
2019-07-08 17:21:07 -0500

    Actualizacion
    se agrego nuevo mensaje

--- src/main/java/com/mycompany/pruebajoobyuno/App.java
+++ src/main/java/com/mycompany/pruebajoobyuno/App.java
@@ -8,7 +8,7 @@ import org.jooby.Jooby;
 public class App extends Jooby {
 
   {
-    get("/", () -> "Hello World!");
+    get("/", () -> "Hola Suemy Solano");
   }
 
   public static void main(final String[] args) {
