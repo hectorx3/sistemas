6fd8964cd5691e7caa3ea6668abcda0c2d89f52c
MayraJuarez <44689127+MayraJuarez@users.noreply.github.com>
2019-07-08 17:35:31 -0500

    Update App.java
    comit 2

--- src/main/java/com/mycompany/pruebajoobyuno/App.java
+++ src/main/java/com/mycompany/pruebajoobyuno/App.java
@@ -8,11 +8,7 @@ import org.jooby.Jooby;
 public class App extends Jooby {
 
   {
-<<<<<<< HEAD
     get("/", () -> "Hello !");
-=======
-    get("/", () -> "SOY THOR DIOS DEL TRUENO!");
->>>>>>> 653c03963737543781bbe00715c1423dd8556a91
   }
 
   public static void main(final String[] args) {
