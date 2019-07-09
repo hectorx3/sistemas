ddf68143607c395f82d8b02e2964c190be7a2c2f
datAlex97 <52679333+datAlex97@users.noreply.github.com>
2019-07-09 16:15:21 -0500

    cambios
    Pues le movi las rutas y no se que mas jejeje

--- src/test/java/com/mycompany/pruebajoobyuno/AppTest.java
+++ src/test/java/com/mycompany/pruebajoobyuno/AppTest.java
@@ -23,20 +23,10 @@ public class AppTest {
 
   @Test
   public void integrationTest() {
-    get("/")
-        .then()
-        .assertThat()
-        .body(equalTo("Hello World!"))
-        .statusCode(200)
-        .contentType("text/html;charset=UTF-8");
   }
 
   @Test
   public void unitTest() throws Throwable {
-    String result = new MockRouter(new App())
-        .get("/");
-
-    assertEquals("Hello World!", result);
   }
 
 }
